0a7c5d3fd1ee9b4224f286a2e813f6ea81072db8
Farel <ninjafarel21.business@gmail.com>
2026-03-25 12:58:06 +0100

    Added async content streaming to the genai test

--- genai_test.py
+++ genai_test.py
@@ -1,12 +1,20 @@
-from google import genai
-from dotenv import load_dotenv
+import asyncio
 import os
 
-load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+from dotenv import load_dotenv
+from google import genai
 
-response = client.models.generate_content(
-    model="gemini-3.1-flash-lite-preview",
-    contents="Explain how AI works in a few words",
-)
-print(response.text)
+
+async def main():
+    load_dotenv()
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY")).aio
+
+    async for chunk in await client.models.generate_content_stream(
+        model="gemini-3.1-flash-lite-preview",
+        contents="Tell me a story in 300 words about the python programming language",
+    ):
+        print(chunk.text, end="")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
